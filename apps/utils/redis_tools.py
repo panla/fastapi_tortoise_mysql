c49136bd68c617c0ccdcfdbc0cd8d7d541da0862
panla <pankla@163.com>
2021-04-03 19:45:53 +0800

    update redis_client

--- apps/utils/redis_tools.py
+++ apps/utils/redis_tools.py
@@ -3,8 +3,15 @@ from aioredis import create_redis_pool
 import config
 
 
+redis_client = None
+
+
 async def register_redis():
     """redis 连接池"""
 
+    global redis_client
     redis_uri = f"redis://:@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_DB}?encoding=utf-8"
-    return await create_redis_pool(redis_uri)
+
+    pool = redis_client or await create_redis_pool(redis_uri)
+    redis_client = pool
+    return pool
