81cb3c3eeb6690d4d972a518c196d9a6969151ed
panla <pankla@163.com>
2021-04-15 10:35:16 +0800

    update tools 完善生成随机字符串的工具

--- tests/resources/v1/admin/test_car.py
+++ tests/resources/v1/admin/test_car.py
@@ -1,9 +1,11 @@
 import random
 
+from starlette.testclient import TestClient
+
 from tests.utils.tools import random_str
 
 
-def test_create_user(client):
+def test_create_user(client: TestClient):
     brand = random_str(19)
     data = {'brand': brand, 'price': random.randint(10000, 100000)}
     r = client.post("/api/v1/admin/cars", json=data)
