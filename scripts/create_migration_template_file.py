c23b7daaf965e56a87b2ba60d7dbf5cb0e461a05
panla <pankla@163.com>
2021-04-21 18:09:10 +0800

    add scripts/create_migration_template_file.py 创建迁移模板文件

---
+++ scripts/create_migration_template_file.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import os
+import sys
+
+migration_dir = os.environ.get('DIR')
+migration_name = os.environ.get('NAME')
+
+if not os.path.isdir(migration_dir):
+    os.makedirs(migration_dir, exist_ok=True)
+
+sql_file_names = os.listdir(migration_dir)
+if sql_file_names:
+    latest_sql_file_index = max([int(f.split('_')[0]) for f in sql_file_names])
+    new_latest_sql_file_index = latest_sql_file_index + 1
+else:
+    new_latest_sql_file_index = 0
+
+now = datetime.now().strftime('%Y%m%d%H%M%S')
+
+new_latest_sql_file_name = f'{new_latest_sql_file_index}_{now}_{migration_name}.sql'
+
+data = """-- upgrade --
+
+-- downgrade --
+"""
+
+with open(f'{migration_dir}/{new_latest_sql_file_name}', 'w', encoding='utf-8') as f:
+    f.write(data)
+
+sys.stdout.write(f'创建 {migration_name} 完成\n')
