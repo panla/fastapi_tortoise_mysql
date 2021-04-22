34ed8cd6503c28a9f76b45f1cc6ead759fd1b2ee
panla <pankla@163.com>
2021-04-22 09:18:12 +0800

    update apps/models/questions.py add import ModelMixin

--- apps/models/question.py
+++ apps/models/question.py
@@ -1,9 +1,9 @@
 from tortoise import fields
 
-from apps.mixins.model import BaseModel
+from apps.mixins.model import BaseModel, ModelMixin
 
 
-class Question(BaseModel):
+class Question(BaseModel, ModelMixin):
     owner = fields.ForeignKeyField('models.User', related_name='questions', db_constraint=False)
     title = fields.CharField(max_length=100, null=False, description='问题')
     content = fields.TextField(description='问题内容')
