c849b6e4d983217535ae9d5dc891465299f7701b
panla <pankla@163.com>
2021-04-28 22:45:28 +0800

    update config and example

--- apps/utils/redis_client.py
+++ apps/utils/redis_client.py
@@ -11,6 +11,6 @@ async def redis_pool():
     global redis_client
     redis_uri = f"redis://:@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_DB}?encoding=utf-8"
 
-    pool = redis_client or await create_redis_pool(redis_uri, password=config.REDIS_PASSWD)
+    pool = redis_client or await create_redis_pool(redis_uri, password=config.REDIS_PASSWD or None)
     redis_client = pool
     return pool
