14fa9842166aa7e7e02c66ce3a78fed9951e67c0
panla <pankla@163.com>
2021-05-10 09:09:58 +0800

    update register_middleware

--- apps/libs/middleare.py
+++ apps/libs/middleare.py
@@ -21,16 +21,11 @@ def register_cross(app: FastAPI):
 def register_middleware(app):
 
     @app.middleware("http")
-    async def add_process_time_header(request: Request, call_next):
+    async def rewrite_other_exception_response(request: Request, call_next):
         response = await call_next(request)
 
-        for middleware_code in middleware_codes:
+        middleware_code = middleware_codes.get(response.status_code, None)
+        if middleware_code:
             code = middleware_code.get('code')
-            if response.status_code == code:
-                content = {
-                    'status_code': middleware_code.get('status_code'),
-                    'message': middleware_code.get('message'),
-                    'data': middleware_code.get('data', None)
-                }
-                return JSONResponse(content=content, status_code=code)
+            return JSONResponse(content=middleware_code, status_code=code)
         return response
--- apps/utils/define.py
+++ apps/utils/define.py
@@ -12,7 +12,9 @@ class StatusCode(object):
 
 
 # 不要和自定义的异常冲突，会覆盖自定义抛出的异常
-middleware_codes = (
-    {'code': 405, 'status_code': 10405, 'message': 'Method Not Allowed', 'data': None},
-    {'code': 413, 'status_code': 10413, 'message': 'REQUEST_ENTITY_TOO_LARGE', 'data': None}
-)
+# 比如，已经自己抛出了 404，就不要在这里定义 404
+
+middleware_codes = {
+    405: {'status_code': 10405, 'message': 'Method Not Allowed', 'data': None},
+    413: {'status_code': 10413, 'message': 'REQUEST_ENTITY_TOO_LARGE', 'data': None}
+}
