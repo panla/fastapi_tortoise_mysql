ab2a4db1dd14a9dbf994918e085b6c2873538848
panla <pankla@163.com>
2021-06-22 10:30:23 +0800

    优化 redis 连接以及 config.py

--- apps/utils/redis_client.py
+++ apps/utils/redis_client.py
@@ -9,11 +9,36 @@ async def redis_pool():
     """redis 连接池"""
 
     global redis_client
-    redis_uri = f"redis://:@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_DB}?encoding=utf-8"
+    redis_uri = f"redis://:{config.REDIS_PASSWD}@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_DB}?encoding=utf-8"
 
-    if config.REDIS_PASSWD:
-        pool = redis_client or await create_redis_pool(redis_uri, password=config.REDIS_PASSWD)
-    else:
-        pool = redis_client or await create_redis_pool(redis_uri)
+    pool = redis_client or await create_redis_pool(redis_uri)
     redis_client = pool
     return pool
+
+
+class RedisToolBase(object):
+
+    _instance = None
+
+    DB = 0
+    PREFIX_KEY = ''
+
+    def __init__(self, key) -> None:
+        self.key = f'{self.PREFIX_KEY}{key}'
+
+    async def init(self):
+        redis_uri = f"redis://:{config.REDIS_PASSWD}@{config.REDIS_HOST}:{config.REDIS_PORT}/{self.db}?encoding=utf-8"
+
+        pool = SMSCodeRedis._instance or await create_redis_pool(redis_uri)
+        SMSCodeRedis._instance = pool
+        self.pool = pool
+
+
+class SMSCodeRedis(RedisToolBase):
+
+    DB = 1
+    PREFIX_KEY = 'sms_code:'
+
+    async def set(self, value):
+        await self.init()
+        await self.pool.set(self.key, value)
--- config.py
+++ config.py
@@ -2,63 +2,40 @@ from starlette.config import Config
 
 config = Config('.env')
 
-CODE_ENV = config('CODE_ENV', default='dev')
+CODE_ENV = config('CODE_ENV', default='prd')
 
 PRD_REDIS_HOST = config('REDIS_HOST', default='127.0.0.1')
 PRD_REDIS_PORT = config('PRD_REDIS_PORT', cast=int, default=6379)
 PRD_REDIS_DB = config('PRD_REDIS_DB', cast=int, default=0)
-PRD_REDIS_USER = config('PRD_REDIS_USER', default='')
 PRD_REDIS_PASSWD = config('PRD_REDIS_PASSWD', default='')
-
-DEV_REDIS_HOST = config('DEV_REDIS_HOST', default='127.0.0.1')
-DEV_REDIS_PORT = config('DEV_REDIS_PORT', cast=int, default=6379)
-DEV_REDIS_DB = config('DEV_REDIS_DB', cast=int, default=0)
-DEV_REDIS_USER = config('DEV_REDIS_USER', default='')
-DEV_REDIS_PASSWD = config('DEV_REDIS_PASSWD', default='')
 
 TEST_REDIS_HOST = config('TEST_REDIS_HOST', default='127.0.0.1')
 TEST_REDIS_PORT = config('TEST_REDIS_PORT', cast=int, default=6379)
 TEST_REDIS_DB = config('TEST_REDIS_DB', cast=int, default=0)
-TEST_REDIS_USER = config('TEST_REDIS_USER', default='')
 TEST_REDIS_PASSWD = config('TEST_REDIS_PASSWD', default='')
 
-PRD_MYSQL_URI = config('PRD_MYSQL_URI')
-DEV_MYSQL_URI = config('DEV_MYSQL_URI')
-TEST_MYSQL_URI = config('TEST_MYSQL_URI')
+PRD_DB_URI = config('PRD_DB_URI')
+TEST_DB_URI = config('TEST_DB_URI')
 
 LOG_LEVEL = config('LOG_LEVEL', default='DEBUG')
 LOG_PATH = config('LOG_PATH')
+INCLUDE_IN_SCHEMA = config('include_in_schema', cast=bool, default=True)
 
-if CODE_ENV == 'prd':
-    REDIS_HOST = PRD_REDIS_HOST
-    REDIS_PORT = PRD_REDIS_PORT
-    REDIS_DB = PRD_REDIS_DB
-    REDIS_USER = PRD_REDIS_USER
-    REDIS_PASSWD = PRD_REDIS_PASSWD
-
-    MYSQL_URI = PRD_MYSQL_URI
-
-    include_in_schema = False
-elif CODE_ENV == 'test':
+if CODE_ENV == 'test':
     REDIS_HOST = TEST_REDIS_HOST
     REDIS_PORT = TEST_REDIS_PORT
     REDIS_DB = TEST_REDIS_DB
-    REDIS_USER = TEST_REDIS_USER
     REDIS_PASSWD = TEST_REDIS_PASSWD
 
-    MYSQL_URI = TEST_MYSQL_URI
+    MYSQL_URI = TEST_DB_URI
 
-    include_in_schema = True
 else:
-    REDIS_HOST = DEV_REDIS_HOST
-    REDIS_PORT = DEV_REDIS_PORT
-    REDIS_DB = DEV_REDIS_DB
-    REDIS_USER = DEV_REDIS_USER
-    REDIS_PASSWD = DEV_REDIS_PASSWD
+    REDIS_HOST = PRD_REDIS_HOST
+    REDIS_PORT = PRD_REDIS_PORT
+    REDIS_DB = PRD_REDIS_DB
+    REDIS_PASSWD = PRD_REDIS_PASSWD
 
-    MYSQL_URI = DEV_MYSQL_URI
-
-    include_in_schema = True
+    MYSQL_URI = PRD_DB_URI
 
 TORTOISE_ORM = {
     'connections': {'default': MYSQL_URI},
@@ -77,3 +54,4 @@ TORTOISE_ORM = {
 
 ADMIN_SECRETS = config('ADMIN_SECRETS')
 TOKEN_EXP_DELTA_ADMIN = config('TOKEN_EXP_DELTA_ADMIN', cast=int, default=86400)
+include_in_schema = INCLUDE_IN_SCHEMA
