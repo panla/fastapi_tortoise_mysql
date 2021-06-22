3bd628a0fdc1ded9de25abdda3678f686828fbcc
panla <pankla@163.com>
2021-06-22 11:25:42 +0800

    update and add 使用 loguru 记录日志

--- apps/utils/log.py
+++ apps/utils/log.py
@@ -1,23 +1,16 @@
 import os
-import logging
-from logging.handlers import TimedRotatingFileHandler
+import sys
+
+from loguru import logger
 
 import config
 
 
-logger = logging.getLogger()
+os.makedirs(os.path.dirname(os.path.abspath(config.LOG_PATH)), exist_ok=True)
 
-log_level = config.LOG_LEVEL.upper()
-logfile_path = config.LOG_PATH
+logger.remove()
 
-os.makedirs(os.path.dirname(logfile_path), exist_ok=True)
-
-file_handler = TimedRotatingFileHandler(logfile_path, 'midnight')
-file_handler.setLevel(level=log_level)
-file_handler.setFormatter(
-    logging.Formatter('[%(asctime)s>] [%(levelname)s] <-%(filename)s-line %(lineno)d>  %(message)s')
-)
-logger.addHandler(file_handler)
-logger.setLevel(level=log_level)
+logger.add(config.LOG_PATH, level=config.LOG_LEVEL.upper(), backtrace=True, diagnose=True, enqueue=True)
+logger.add(sys.stdout, level=config.LOG_LEVEL.upper(), backtrace=True, diagnose=True, enqueue=True)
 
 __all__ = ['logger']
