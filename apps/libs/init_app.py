7f1ad0d3f01f4160565ca16f1f9c049f8f923257
panla <pankla@163.com>
2021-07-02 16:05:25 +0800

    恢复，绑定子 app

--- apps/libs/init_app.py
+++ apps/libs/init_app.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from apps.libs import register_cross
 from apps.libs.database import init_db
 
-# from apps.v1_admin import init_sub_app as init_v1_admin_app
-# from apps.v1_socket_io import init_sub_app as init_v1_socket_io_app
+from apps.v1_admin import init_sub_app as init_v1_admin_app
+from apps.v1_socket_io import init_sub_app as init_v1_socket_io_app
 
 
 def init_app(app: FastAPI):
@@ -13,5 +13,5 @@ def init_app(app: FastAPI):
     init_db(app)
     register_cross(app)
 
-    # init_v1_admin_app(app)
-    # init_v1_socket_io_app(app)
+    init_v1_admin_app(app)
+    init_v1_socket_io_app(app)
