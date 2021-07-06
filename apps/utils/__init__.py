e6374cc6bcee0d37fbafd2786fefae2e8337202a
panla <pankla@163.com>
2021-07-06 13:38:21 +0800

    move apps/utils/validators.py -> apps/extension/validators.py

--- apps/utils/__init__.py
+++ apps/utils/__init__.py
@@ -3,5 +3,4 @@ from .exception import UnauthorizedException, ForbiddenException, NotFoundExcept
 from .response import resp_success, raise_400, raise_401, raise_403, raise_404
 from .response import error_response
 from .tools import random_str, random_int
-from .validators import MaxMinValidator
 from .log import logger
