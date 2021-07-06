4c082beb8e00d98bc5b42dee4829ecb798fdb555
panla <pankla@163.com>
2021-07-06 13:38:52 +0800

    add customize Model Field

--- apps/extension/__init__.py
+++ apps/extension/__init__.py
@@ -1,1 +1,3 @@
 from .route import Route
+from .validators import MaxMinValidator
+from .fields import UnsignedTinyIntField, UnsignedSmallIntField, UnsignedIntField, UnsignedBigIntField
---
+++ apps/extension/fields.py
@@ -0,0 +1,83 @@
+from typing import Any
+
+from tortoise.fields.base import Field
+
+
+class UnsignedTinyIntField(Field, int):
+    SQL_TYPE = "TINYINT UNSIGNED"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else 0,
+            "le": 255,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT"
+
+
+class UnsignedSmallIntField(Field, int):
+    SQL_TYPE = "SMALLINT UNSIGNED"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else 0,
+            "le": 65535,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT"
+
+
+class UnsignedIntField(Field, int):
+    SQL_TYPE = "INT UNSIGNED"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else 0,
+            "le": 4294967295,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT"
+
+
+class UnsignedBigIntField(Field, int):
+    SQL_TYPE = "BIGINT UNSIGNED"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else 0,
+            "le": 18446744073709551615,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT"
