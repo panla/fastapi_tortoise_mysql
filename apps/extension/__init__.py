1a71e79a30af6880d1d82ead71d90da2d254bdb0
panla <pankla@163.com>
2021-07-13 17:37:44 +0800

    add 增加数据库扩展字段

--- apps/extension/__init__.py
+++ apps/extension/__init__.py
@@ -1,4 +1,19 @@
 from .route import Route
 from .validators import MaxMinValidator
-from .fields import UnsignedTinyIntField, UnsignedSmallIntField, UnsignedIntField, UnsignedBigIntField
-from .exceptions import BaseHTTPException, BadRequest, Unauthorized, Forbidden, NotFound, MethodNotAllowed
+from .fields import (
+    TinyIntField,
+    MediumIntField,
+    UnsignedTinyIntField,
+    UnsignedSmallIntField,
+    UnsignedMediumIntField,
+    UnsignedIntField,
+    UnsignedBigIntField,
+)
+from .exceptions import (
+    BaseHTTPException,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    MethodNotAllowed
+)
--- apps/extension/fields.py
+++ apps/extension/fields.py
@@ -1,5 +1,6 @@
 __all__ = [
-    'UnsignedTinyIntField', 'UnsignedSmallIntField', 'UnsignedIntField', 'UnsignedBigIntField',
+    'TinyIntField', 'MediumIntField', 'UnsignedTinyIntField', 'UnsignedSmallIntField', 'UnsignedMediumIntField',
+    'UnsignedIntField', 'UnsignedBigIntField',
 ]
 
 from typing import Any
@@ -7,7 +8,68 @@ from typing import Any
 from tortoise.fields.base import Field
 
 
+class TinyIntField(Field, int):
+    """
+    Tiny integer field. (8-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
+    SQL_TYPE = "TINYINT"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else -128,
+            "le": 127,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT"
+
+
+class MediumIntField(Field, int):
+    """
+    Medium integer field. (24-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
+    SQL_TYPE = "MEDIUMINT"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else -8388608,
+            "le": 8388607,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "MEDIUMINT NOT NULL PRIMARY KEY AUTO_INCREMENT"
+
+
 class UnsignedTinyIntField(Field, int):
+    """
+    Unsigned Tiny integer field. (8-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
     SQL_TYPE = "TINYINT UNSIGNED"
     allows_generated = True
 
@@ -28,6 +90,13 @@ class UnsignedTinyIntField(Field, int):
 
 
 class UnsignedSmallIntField(Field, int):
+    """
+    Unsigned Small integer field. (16-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
     SQL_TYPE = "SMALLINT UNSIGNED"
     allows_generated = True
 
@@ -47,7 +116,41 @@ class UnsignedSmallIntField(Field, int):
         GENERATED_SQL = "SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT"
 
 
+class UnsignedMediumIntField(Field, int):
+    """
+    Unsigned Medium integer field. (24-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
+    SQL_TYPE = "MEDIUMINT UNSIGNED"
+    allows_generated = True
+
+    def __init__(self, pk: bool = False, **kwargs: Any) -> None:
+        if pk:
+            kwargs["generated"] = bool(kwargs.get("generated", True))
+        super().__init__(pk=pk, **kwargs)
+
+    @property
+    def constraints(self) -> dict:
+        return {
+            "ge": 1 if self.generated or self.reference else 0,
+            "le": 16777215,
+        }
+
+    class _db_mysql:
+        GENERATED_SQL = "MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT"
+
+
 class UnsignedIntField(Field, int):
+    """
+    Unsigned Int integer field. (32-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
     SQL_TYPE = "INT UNSIGNED"
     allows_generated = True
 
@@ -68,6 +171,13 @@ class UnsignedIntField(Field, int):
 
 
 class UnsignedBigIntField(Field, int):
+    """
+    Unsigned Big integer field. (64-bit unsigned)
+
+    ``pk`` (bool):
+        True if field is Primary Key.
+    """
+
     SQL_TYPE = "BIGINT UNSIGNED"
     allows_generated = True
 
