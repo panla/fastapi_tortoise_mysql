b6e783b16a81baf1af56bb6bda17f9a99f8cee90
panla <pankla@163.com>
2021-07-28 18:20:55 +0800

    update random_str random_int by python.string

--- apps/utils/tools.py
+++ apps/utils/tools.py
@@ -3,7 +3,7 @@ __all__ = [
 ]
 
 import random
-from typing import Union
+import string
 
 
 def random_str(length: int = 20, has_num: bool = False) -> str:
@@ -14,22 +14,14 @@ def random_str(length: int = 20, has_num: bool = False) -> str:
     :return: str
     """
 
-    lis = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    ]
+    all_char = string.ascii_lowercase + string.ascii_uppercase
     if has_num:
-        lis += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        all_char += string.digits
 
-    results = []
-    for i in range(length):
-        results += random.choices(lis)
-    return ''.join(results)
+    return ''.join(random.sample(all_char, length))
 
 
-def random_int(length: int = 4, is_int: bool = False) -> Union[str, int]:
+def random_int(length: int = 4) -> str:
     """
     generate a random str/int and len == length
     :param length: 指定的长度，默认4
@@ -37,11 +29,5 @@ def random_int(length: int = 4, is_int: bool = False) -> Union[str, int]:
     :return: Union[str, int]
     """
 
-    lis = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    results = []
-    for i in range(length):
-        results += random.choices(lis)
-    results = ''.join(results)
-    if is_int:
-        return int(results)
-    return results
+    all_char = string.digits
+    return ''.join(random.sample(all_char, length))
