d38c029455bf554707c28a55a692cde713146712
panla <pankla@163.com>
2021-07-29 21:59:56 +0800

    update run.example.sh and add gunicorn_settings_example.py

---
+++ doc/config/gunicorn_settings_example.py
@@ -0,0 +1,21 @@
+"""
+gunicorn 配置文件示例
+参考
+    https://docs.gunicorn.org/en/stable/settings.html
+"""
+
+bind = '0.0.0.0:8000'
+
+workers = 2
+
+worker_class = 'tools.worker.Worker'
+
+worker_connections = 10000
+
+timeout = 120
+
+graceful_timeout = 20
+
+keepalive = 20
+
+loglevel = 'INFO'
--- doc/config/run.example.sh
+++ doc/config/run.example.sh
@@ -1,2 +1,2 @@
 aerich upgrade
-gunicorn main:app --bind 0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker --workers 2 --worker-connections 10000 --log-level INFO --timeout 60
+gunicorn main:app --bind 0.0.0.0:8000 --workers 2 --worker-class uvicorn.workers.UvicornWorker --worker-connections 10000 --timeout 120 --graceful-timeout 20 --keep-alive 20 --log-level INFO
