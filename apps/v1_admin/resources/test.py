bb30dae93f290a8864b0dbc562e605db2fdf730c
panla <pankla@163.com>
2021-07-30 18:56:59 +0800

    update API tests -> tests/speed/common

--- apps/v1_admin/resources/test.py
+++ apps/v1_admin/resources/test.py
@@ -6,7 +6,7 @@ from apps.utils import resp_success
 router = APIRouter(route_class=Route)
 
 
-@router.get('', status_code=200, responses=error_response)
+@router.get('/speed/common', status_code=200, responses=error_response)
 async def test_speed_common():
     """the api of test wrk speed"""
 
