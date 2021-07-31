f5e0f07dc63c0c00cc2af1b476873c5ebd09b519
panla <pankla@163.com>
2021-08-01 00:19:40 +0800

    add API /api/v1/admin/tests/speed/cars/{c_id}

--- apps/v1_admin/resources/test.py
+++ apps/v1_admin/resources/test.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 
-from apps.extensions import Route, error_response
+from apps.extensions import Route, NotFound, error_response
 from apps.utils import resp_success
+from apps.models import Car
+from apps.v1_admin.entities import ReadCarSchema
 
 router = APIRouter(route_class=Route)
 
@@ -11,3 +13,15 @@ async def test_speed_common():
     """the api of test wrk speed"""
 
     return resp_success(data={'success': True})
+
+
+@router.get('/speed/cars/{c_id}', response_model=ReadCarSchema, status_code=200, responses=error_response)
+async def read_car(
+        c_id: int = Path(..., description='汽车id', ge=1)
+):
+    """the api of read one car"""
+
+    car = await Car.get_or_none(id=c_id, is_delete=False)
+    if car:
+        return resp_success(data=car)
+    raise NotFound(message=f'Car {c_id} not exists')
