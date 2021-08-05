bf079bd60978b3fc3486aac13da757cb50366320
panla <pankla@163.com>
2021-08-05 09:54:03 +0800

    fix bug redis client

--- apps/redis_ext/base.py
+++ apps/redis_ext/base.py
@@ -8,13 +8,15 @@ class RedisToolBase(object):
 
     DB = 0
     PREFIX_KEY = ''
+    CONNECTION_PARAMS = {'encoding': 'utf-8', 'decode_responses': True}
 
     def __init__(self, key) -> None:
         self.key = f'{self.PREFIX_KEY}{key}'
-        self.redis_uri = 'redis://:{}@{}:{}/{}?encoding=utf-8'.format(
+        self.uri = 'redis://:{}@{}:{}/{}'.format(
             Config.REDIS_PASSWD, Config.REDIS_HOST, Config.REDIS_PORT, self.DB
         )
 
-    async def init(self):
-        self._client = self._client or aioredis.from_url(self.redis_uri, decode_responses=True)
+    @property
+    def client(self):
+        self._client = self._client or aioredis.from_url(self.uri, **self.CONNECTION_PARAMS)
         return self._client
--- apps/redis_ext/operator.py
+++ apps/redis_ext/operator.py
@@ -6,10 +6,8 @@ class SMSCodeRedis(RedisToolBase):
     PREFIX_KEY = 'sms_code:'
 
     async def set(self, value):
-        client = await self.init()
-        await client.set(self.key, value)
+        await self.client.set(self.key, value)
 
     async def get(self):
-        client = await self.init()
-        rt = await client.get(self.key)
+        rt = await self.client.get(self.key)
         return rt
