687b78e33a49b0adada5e0defa6f9e233d5dc58c
panla <pankla@163.com>
2022-09-23 13:42:36 +0800

    fix and update MqttClient on_xxxx function

--- services/mqtt_services/client.py
+++ services/mqtt_services/client.py
@@ -4,12 +4,13 @@ pip install paho-mqtt
 
 import json
 import ssl
-from typing import Any
+from typing import Any, List
 
 from paho.mqtt.client import Client, MQTTv311, MQTTv5
 from paho.mqtt.subscribeoptions import SubscribeOptions
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes
+from paho.mqtt.reasoncodes import ReasonCodes
 
 from extensions import logger
 
@@ -35,7 +36,7 @@ class MqttClient:
         Args:
             host (str): MQTT Server Host.
             port (int): MQTT Server Port.
-            client_id (str): Client ClienID.
+            client_id (str): Client ClientID.
             username (str): Client UserName.
             password (str): Client Password.
             keep_alive (int, optional): MQTT Connect KeepAlive.
@@ -63,21 +64,28 @@ class MqttClient:
         self.unique = self.client_id or self.username
 
         self.client = Client(client_id=self.client_id, protocol=version)
-        self.client.on_connect = self.on_connect
-        self.client.on_disconnect = self.on_disconnect
-        self.client.on_subscribe = self.on_subscribe
-        self.client.on_unsubscribe = self.on_unsubscribe
-        # self.client.on_publish = self.on_publish
+        if version == MQTTv5:
+            self.client.on_connect = self.on_connect_v5
+            self.client.on_disconnect = self.on_disconnect_v5
+            self.client.on_subscribe = self.on_subscribe_v5
+            self.client.on_unsubscribe = self.on_unsubscribe_v5
+        else:
+            self.client.on_connect = self.on_connect_v3
+            self.client.on_disconnect = self.on_disconnect_v3
+            self.client.on_subscribe = self.on_subscribe_v3
+            self.client.on_unsubscribe = self.on_unsubscribe_v3
+
+        self.client.on_publish = self.on_publish
         self.client.on_message = self.on_message
 
     def set_ssl_context(self):
         """set SSL context to use SSL"""
 
-        if self.ca_cert and self.client_cert and self.client_key:
+        if self._ca_cert and self._cert_path and self._key_path:
             context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS)
             context.check_hostname = False
-            context.load_cert_chain(certfile=self.client_cert, keyfile=self.client_key, password=self.keyfile_passwd)
-            context.load_verify_locations(self.ca_cert)
+            context.load_cert_chain(certfile=self._cert_path, keyfile=self._key_path, password=self._keyfile_passwd)
+            context.load_verify_locations(self._ca_cert)
             context.verify_mode = ssl.CERT_REQUIRED
 
             self.client.tls_set_context(context=context)
@@ -98,9 +106,9 @@ class MqttClient:
         self.client.username_pw_set(self.username, self.password)
         self.set_ssl_context()
 
-        return self.client.connect(self.host, self.port, self.keep_alive, properties=self.properties)
+        return self.client.connect(self._host, self._port, self._keep_alive, properties=self.properties)
 
-    def disconnect(self, rc: int = None):
+    def disconnect(self, rc: ReasonCodes = None):
         """Disconnect a connected client from the broker.
         rc: (MQTT v5.0 only) a ReasonCodes instance setting the MQTT v5.0
         rc to be sent with the disconnect.  It is optional, the receiver
@@ -114,15 +122,15 @@ class MqttClient:
 
         self.client.loop_start()
 
-    def loop_stop(self):
+    def loop_stop(self, force: bool = False):
         """loop stop"""
 
-        self.client.loop_stop()
+        self.client.loop_stop(force=force)
 
-    def loop_forever(self):
+    def loop_forever(self, timeout: float = 1.0):
         """loop forever"""
 
-        self.client.loop_forever()
+        self.client.loop_forever(timeout=timeout)
 
     def add_callback(self, topic, callback):
         """Register a message callback for a specific topic."""
@@ -155,34 +163,61 @@ class MqttClient:
             topic=topic, payload=payload, qos=qos, retain=retain, properties=self.properties
         )
 
-    def on_connect(self, client: Client, userdata: Any, flags: dict, rc: int, properties: Properties = None):
+    def on_connect_v3(self, client: Client, userdata: Any, flags: dict, rc: int):
         """Define the connected callback implementation."""
 
-        logger.info(f'{self.unique} on_connect flags {flags} result code: {rc}'.center(60, '*'))
+        logger.info(f'{self.unique} on_connect flags {flags} rc {rc}'.center(60, '*'))
 
-    def on_disconnect(self, client: Client, userdata: Any, rc: int, properties: Properties = None):
+    def on_connect_v5(
+            self, client: Client, userdata: Any, flags: dict, reason_code: ReasonCodes, properties: Properties
+    ):
+        """Define the connected callback implementation."""
+
+        logger.info(f'{self.unique} on_connect flags {flags} rc {reason_code}'.center(60, '*'))
+
+    def on_disconnect_v3(self, client: Client, userdata: Any, rc: int):
         """If implemented, called when the client disconnects from the broker."""
 
         logger.info(f'{self.unique} on_disconnected rc {rc}'.center(60, '*'))
 
-    def on_subscribe(self, client: Client, userdata: Any, mid: int, granted_qos: tuple, properties: Properties = None):
+    def on_disconnect_v5(self, client: Client, userdata: Any, rc: int, properties: Properties = None):
+        """If implemented, called when the client disconnects from the broker."""
+
+        logger.info(f'{self.unique} on_disconnected rc {rc}'.center(60, '*'))
+
+    def on_subscribe_v3(self, client: Client, userdata: Any, mid: int, granted_qos: tuple):
         """Define the subscribed callback implementation."""
 
-        logger.info(f'{self.unique} on_subscribe: mid {mid} granted_qos = {granted_qos}'.center(60, '*'))
+        logger.info(f'{self.unique} on_subscribe: mid {mid} granted_qos {granted_qos}'.center(60, '*'))
 
-    def on_unsubscribe(self, client: Client, userdata: Any, mid: int, properties: Properties = None):
-        """Define the unsubscribe callback implementation."""
+    def on_subscribe_v5(
+            self, client: Client, userdata: Any, mid: int, reason_codes: List[ReasonCodes], properties: Properties
+    ):
+        """Define the subscribed callback implementation."""
 
-        logger.info(f'{self.unique} on_unsubscribe: mid = {mid}'.center(60, '*'))
+        _reason_codes = [rc.json() for rc in reason_codes]
+        logger.info(f'{self.unique} on_subscribe: mid {mid} reason_codes {_reason_codes}'.center(60, '*'))
 
-    def on_publish(self, client: Client, userdata: Any, mid: int, properties: Properties = None):
+    def on_unsubscribe_v3(self, client: Client, userdata: Any, mid: int):
+        """Define the un subscribe callback implementation."""
+
+        logger.info(f'{self.unique} on_unsubscribe: mid {mid}'.center(60, '*'))
+
+    def on_unsubscribe_v5(
+            self, client: Client, userdata: Any, mid: int, properties: Properties, reason_code: ReasonCodes
+    ):
+        """Define the un subscribe callback implementation."""
+
+        logger.info(f'{self.unique} on_unsubscribe: mid {mid} reason_code {reason_code}'.center(60, '*'))
+
+    def on_publish(self, client: Client, userdata: Any, mid: int):
         """Define the published message callback implementation."""
 
-        logger.info(f'{self.unique} on_publish: mid = {mid}'.center(60, '*'))
+        logger.info(f'{self.unique} on_publish: mid {mid}'.center(60, '*'))
 
-    def on_message(self, client: Client, userdata: Any, msg, properties: Properties = None):
+    def on_message(self, client: Client, userdata: Any, message):
         """Define the message received callback implementation. use this when can`t match message_callback_add"""
 
-        logger.info(f'{self.unique} on_message topic {msg.topic}'.center(60, '*'))
-        payload = msg.payload.decode('utf-8')
+        logger.info(f'{self.unique} on_message topic {message.topic}'.center(60, '*'))
+        payload = message.payload.decode('utf-8')
         logger.info(payload)
